5f044949c41055ae7e1c3b0e21332cd89d465f0c
Jose Sebastian Torralba <SebastianTorralba@coder.com.ar>
2024-11-01 10:17:49 -0300

    Ingreso por teclado

--- clase2/src/main/java/ar/edu/unlar/paradigmas1/App.java
+++ clase2/src/main/java/ar/edu/unlar/paradigmas1/App.java
@@ -4,6 +4,7 @@ import ar.edu.unlar.paradigmas1.objetos.Alumno;
 import ar.edu.unlar.paradigmas1.objetos.Persona;
 import ar.edu.unlar.paradigmas1.objetos.Profesor;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Hello world!
@@ -30,13 +31,13 @@ public class App {
 
         System.out.println(p);
         System.out.println(a);
-        
+
         Profesor profe = new Profesor();
         profe.setApellido("Lujan");
         profe.setNombre("Adrian");
         profe.setLegajo("1234");
         profe.setDocumentoNumero("30123200");
-        
+
         ArrayList<Persona> personas = new ArrayList<Persona>();
         personas.add(p);
         personas.add(a);
@@ -49,6 +50,37 @@ public class App {
             }
             System.out.println(persona.getClass().getSimpleName());
         }
+        // Ingresar por teclado 5 Alumnos y mostrarlos en pantalla
+        Scanner teclado = new Scanner(System.in);
+        ArrayList<Alumno> alumnos = new ArrayList<Alumno>();
+        for (int i = 0; i < 5; i++) {
+            alumnos.add(
+                    cargaAlumno(teclado));
+        }
+        
+        for (Alumno alumno : alumnos) {
+            System.out.println("Alumno"+alumno.getNombre());
+        }
 
+    }
+
+    public static Alumno cargaAlumno(Scanner teclado) {
+        Alumno alumno = new Alumno();
+        System.out.println("Ingrese Nombre");
+        alumno.setNombre(teclado.next());
+        System.out.println("Ingrese Apellido");
+        alumno.setApellido(teclado.next());
+        System.out.println("Ingrese Documento");
+        alumno.setDocumentoNumero(teclado.next());
+        System.out.println("Ingrese Libreta");
+        alumno.setMatricula(teclado.next());
+        System.out.println("Ingrese si cursa actualmente S/N");
+        String respuesta = teclado.next();
+        while (!respuesta.equals("S")) {
+            System.out.println("Ingrese nuevamente");
+            respuesta = teclado.next();
+        }
+        alumno.setCursaActualmente(true);
+        return alumno;
     }
 }
